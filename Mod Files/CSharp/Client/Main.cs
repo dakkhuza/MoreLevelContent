970aa8e17de8f799dd3293956b8ec5885e0f83ad
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-01 20:06:51 -0700

    Remove this old, outdated config stuff

--- Mod Files/CSharp/Client/Main.cs
+++ Mod Files/CSharp/Client/Main.cs
@@ -20,7 +20,6 @@ namespace MoreLevelContent
     partial class Main
     {
         private GUIButton SettingsButton;
-        private FieldInfo traitorProbabilityText;
         public void InitClient()
         {
             MapUI.Instance.Setup();
@@ -30,12 +29,6 @@ namespace MoreLevelContent
 
             MethodInfo info = typeof(GUI).GetMethod("TogglePauseMenu", BindingFlags.Static | BindingFlags.Public);
             Patch(info, postfix: new HarmonyMethod(AccessTools.Method(typeof(Main), "AddSettingsButton")));
-
-            // or single player
-            if (GameMain.IsSingleplayer) return;
-
-            traitorProbabilityText = typeof(NetLobbyScreen).GetField("traitorProbabilityText", BindingFlags.Instance | BindingFlags.NonPublic);
-            //CreateSettingsButton();
         }
 
 
@@ -52,24 +45,6 @@ namespace MoreLevelContent
                     return Instance.OpenConfig(obj, o); 
                 },
             };
-        }
-
-        private void CreateSettingsButton()
-        {
-            if (SettingsButton != null) return; // Exit if the settings button is already created
-            GUITextBlock textBox =  (GUITextBlock)traitorProbabilityText.GetValue(GameMain.NetLobbyScreen);
-            var settingsContentRect = textBox.RectTransform
-                .Parent // traitorProbContainer
-                .Parent // traitorsSettingHolder
-                .Parent // settingsContent
-                ;
-            var mlcSettingHolder = new GUILayoutGroup(new RectTransform(new Vector2(1.0f, 0.1f), settingsContentRect), isHorizontal: true, childAnchor: Anchor.CenterLeft) { Stretch = true };
-            SettingsButton = new GUIButton(new RectTransform(new Vector2(0.25f, 1.0f), mlcSettingHolder.RectTransform, Anchor.TopRight),
-                TextManager.Get("mlc.config"))
-            {
-                OnClicked = OpenConfig
-            };
-            Log.Debug("Created settings button");
         }
 
         private bool OpenConfig(GUIButton button, object obj)

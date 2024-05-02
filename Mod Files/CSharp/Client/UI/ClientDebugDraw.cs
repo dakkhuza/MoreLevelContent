cb0a995397781e52734dfa742af9562c994160b8
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-01 20:41:42 -0700

    Add some basic debug drawing

---
+++ Mod Files/CSharp/Client/UI/ClientDebugDraw.cs
@@ -0,0 +1,16 @@
+﻿using Barotrauma;
+using Microsoft.Xna.Framework.Graphics;
+using MoreLevelContent.Shared.Generation;
+using Microsoft.Xna.Framework;
+
+public static class ClientDebugDraw
+{
+    internal static void Draw(SpriteBatch spriteBatch, Camera cam)
+    {
+        foreach (var item in CaveGenerationDirector.Instance._InitialCaveCheckDebug)
+        {
+            GUI.DrawString(spriteBatch, new Vector2(item.Cell.Center.X, -item.Cell.Center.Y), "Cell", Color.Azure);
+        }
+        if (CaveGenerationDirector.Instance.ActiveThalaCave != null) CaveGenerationDirector.Instance.ActiveThalaCave.DebugDraw(spriteBatch, cam);
+    }
+}

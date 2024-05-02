f61f4eeff199658e27037b33892fa360107665c9
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-01 19:42:35 -0700

    Added a debug hook for drawing debug info

--- Mod Files/CSharp/Shared/Utils/Hooks.cs
+++ Mod Files/CSharp/Shared/Utils/Hooks.cs
@@ -4,18 +4,34 @@ using System.Reflection;
 using System;
 using Barotrauma.MoreLevelContent.Shared.Utils;
 
+#if CLIENT
+using Microsoft.Xna.Framework.Graphics;
+#endif
+
 namespace MoreLevelContent.Shared.Utils
 {
     internal class Hooks : Singleton<Hooks>
     {
         internal event Action<Structure, float, Character> OnStructureDamaged;
+#if CLIENT
+        internal event Action<SpriteBatch, Camera> OnDebugDraw;
+#endif
+
         public override void Setup()
         {
             var Structure_SetDamage = typeof(HumanAIController).GetMethod(nameof(HumanAIController.StructureDamaged), BindingFlags.Public | BindingFlags.Static);
             _ = Main.Harmony.Patch(Structure_SetDamage, prefix: new HarmonyMethod(typeof(Hooks), nameof(StructureDamaged)));
+#if CLIENT
+            var Level_DrawDebugOverlay = typeof(Level).GetMethod(nameof(Level.DrawDebugOverlay));
+            _ = Main.Harmony.Patch(Level_DrawDebugOverlay, postfix: new HarmonyMethod(typeof(Hooks), nameof(DebugDraw)));
+#endif
             Log.Error("Patched Hooks");
         }
 
         private static void StructureDamaged(Structure structure, float damageAmount, Character character) => Instance.OnStructureDamaged?.Invoke(structure, damageAmount, character);
+
+#if CLIENT
+        private static void DebugDraw(SpriteBatch spriteBatch, Camera cam) => Instance.OnDebugDraw?.Invoke(spriteBatch, cam);
+#endif
     }
 }

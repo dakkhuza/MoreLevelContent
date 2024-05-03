d8e9b86e88ebc19c7c880e6dad2bfdf56b4b996d
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-02 22:16:31 -0700

    Debug draw for safe area

--- Mod Files/CSharp/Client/UI/ClientDebugDraw.cs
+++ Mod Files/CSharp/Client/UI/ClientDebugDraw.cs
@@ -3,11 +3,19 @@ using Microsoft.Xna.Framework.Graphics;
 using MoreLevelContent.Shared.Generation;
 using Microsoft.Xna.Framework;
 using MoreLevelContent.Shared.Utils;
+using Barotrauma.Items.Components;
+using MoreLevelContent.Shared;
+using System;
 
 public static class ClientDebugDraw
 {
     internal static void Draw(SpriteBatch spriteBatch, Camera cam)
     {
+        if (Level.Loaded != null)
+        {
+            spriteBatch.DrawCircle(new Vector2(Level.Loaded.StartPosition.X, -Level.Loaded.StartPosition.Y), CaveGenerationDirector.MIN_DIST_FROM_START, 16, Color.Red, thickness: 100);
+        }
+
         foreach (var item in CaveGenerationDirector.Instance._InitialCaveCheckDebug)
         {
             GUI.DrawString(spriteBatch, new Vector2(item.Cell.Center.X, -item.Cell.Center.Y), "Cell", Color.Azure);

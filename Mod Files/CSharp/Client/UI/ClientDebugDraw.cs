077648c231e758ba4760083a549a79eaafa0cd4f
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-05 18:56:27 -0700

    Revert "Remove unused turret extensions"
    This reverts commit 6cf8c8f6ad3b7d5c892a7db57c15709cabe68324.

--- Mod Files/CSharp/Client/UI/ClientDebugDraw.cs
+++ Mod Files/CSharp/Client/UI/ClientDebugDraw.cs
@@ -21,6 +21,13 @@ public static class ClientDebugDraw
         {
             GUI.DrawString(spriteBatch, new Vector2(item.Cell.Center.X, -item.Cell.Center.Y), "Cell", Color.Azure);
         }
+        if (TurretExtensions.TargetHull != default)
+        {
+            GUI.DrawRectangle(spriteBatch,
+                TurretExtensions.TargetHull, 
+                Color.Yellow, thickness: 2);
+        }
+        GUI.DrawRectangle(spriteBatch, TurretExtensions.Hit, 20, 20, 0, Color.Orange, thickness: 10);
         if (CaveGenerationDirector.Instance.ActiveThalaCave != null) CaveGenerationDirector.Instance.ActiveThalaCave.DebugDraw(spriteBatch, cam);
     }
 }

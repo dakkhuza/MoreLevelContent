12a690c220f3cb7b370724855f08e90460a08598
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-10 05:10:29 -0700

    Implement "AddNewsStory" to map director

--- Mod Files/CSharp/Client/Generation/Map/MapDirector.cs
+++ Mod Files/CSharp/Client/Generation/Map/MapDirector.cs
@@ -1,20 +1,47 @@
 ﻿using Barotrauma.MoreLevelContent.Shared.Utils;
 using MoreLevelContent.Networking;
+using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using Barotrauma;
 
 namespace MoreLevelContent.Shared.Generation
 {
     // Client
     public partial class MapDirector : Singleton<MapDirector>
     {
+        private FieldInfo _notificationList;
+        private Type _notification;
+        private MethodInfo _addMethod;
+        private ConstructorInfo _notifConstructor;
         partial void SetupProjSpecific()
         {
             NetUtil.Register(NetEvent.MAP_CONNECTION_EQUALITYCHECK_SENDCLIENT, ConnectionEqualityCheck);
+            _notificationList = AccessTools.Field(typeof(Map), "mapNotifications");
+            _notification = typeof(Map).GetNestedType("MapNotification", BindingFlags.NonPublic);
 
+            _addMethod = AccessTools.Method(_notificationList.FieldType, "Add");
+            _notifConstructor = AccessTools.Constructor(_notification, new Type[] { typeof(string), typeof(GUIFont), _notificationList.FieldType, typeof(Location) });
+        }
+
+        public void AddNewsStory(string message)
+        {
+            object list = _notificationList.GetValue(GameMain.GameSession.Map);
+
+            var notification = _notifConstructor.Invoke(new object[] {
+                message,
+                GUIStyle.SubHeadingFont,
+                list,
+                null
+            });
+
+            _ = _addMethod.Invoke(list, new object[] { notification });
+
+            _notificationList.SetValue(GameMain.GameSession.Map, list);
         }
     }
 }

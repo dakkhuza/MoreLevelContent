daf1a729de186fa19db2db932a2b526a465409fd
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-26 00:03:28 -0700

    Create MapFeatureModule.cs

---
+++ Mod Files/CSharp/Shared/Generation/Map/Modules/MapFeatureModule.cs
@@ -0,0 +1,16 @@
+﻿using Barotrauma;
+using MoreLevelContent.Missions;
+using MoreLevelContent.Shared.Data;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System;
+using System.Linq;
+
+namespace MoreLevelContent.Shared.Generation
+{
+    internal partial class MapFeatureModule : MapModule
+    {
+        protected override void InitProjSpecific() { }
+
+    }
+}

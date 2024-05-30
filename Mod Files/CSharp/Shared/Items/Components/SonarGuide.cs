9f0acfa680743ce628f0e421d4163451222131c4
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-05-30 02:05:01 -0700

    Sonar guides now reveal the map feature when close

--- Mod Files/CSharp/Shared/Items/Components/SonarGuide.cs
+++ Mod Files/CSharp/Shared/Items/Components/SonarGuide.cs
@@ -2,7 +2,11 @@
 using Barotrauma.Items.Components;
 using Microsoft.Xna.Framework;
 using MoreLevelContent.Shared;
+using MoreLevelContent.Shared.Data;
+using MoreLevelContent.Shared.Generation;
 using MoreLevelContent.Shared.Utils;
+using System;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace MoreLevelContent.Items
@@ -12,17 +16,35 @@ namespace MoreLevelContent.Items
         [Serialize(10.0f, IsPropertySaveable.Yes, description: "How often the guide sends out a ping."), Editable]
         public float PingInterval { get; private set; }
 
-        [Serialize(30000f, IsPropertySaveable.Yes, description: "How far away this guide can be detected from, 10000.0f is the default sonar range."), Editable]
+        [Serialize(30000.0f, IsPropertySaveable.Yes, description: "How far away this guide can be detected from, 10000.0f is the default sonar range."), Editable]
         public float Range { get; private set; }
+
+        [Serialize(true, IsPropertySaveable.Yes, description: "If getting near this guide should reveal the levels map feature."), Editable]
+        public bool RevealMapFeature { get; private set; }
+
+        [Serialize(10000.0f, IsPropertySaveable.Yes, description: "How close a player has to be before the map feature is revealed."), Editable]
+        public float RevealRange { get; private set; }
+
         private float _Interval;
+        private bool _Revealed;
         public SonarGuide(Item item, ContentXElement element) : base(item, element)
         {
             IsActive = true;
+            _Revealed = false;
         }
 
         public override void Update(float deltaTime, Camera cam)
         {
             UpdateOnActiveEffects(deltaTime);
+
+            if (RevealMapFeature && !_Revealed && Level.Loaded != null)
+            {
+                if (GameSession.GetSessionCrewCharacters(CharacterType.Player).Any(c => Vector2.DistanceSquared(c.WorldPosition, item.WorldPosition) < MathUtils.Pow2(RevealRange)))
+                {
+                    Level.Loaded.LevelData.MLC().MapFeatureData.Revealed = true;
+                    _Revealed = true;
+                }
+            }
 
 #if CLIENT
             if (Voltage >= MinVoltage)

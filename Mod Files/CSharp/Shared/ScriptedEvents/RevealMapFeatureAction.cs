bc68dfa7d7efbf6c2a838ff5bce0d3c0da1e3cd6
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-06-06 15:13:53 -0700

    Create RevealMapFeatureAction.cs

---
+++ Mod Files/CSharp/Shared/ScriptedEvents/RevealMapFeatureAction.cs
@@ -0,0 +1,31 @@
+﻿
+using Barotrauma;
+using MoreLevelContent.Shared.Data;
+using MoreLevelContent.Shared.Generation;
+
+namespace MoreLevelContent
+{
+    /// <summary>
+    /// Changes what map feature the current level has.
+    /// </summary>
+    internal class RevealMapFeatureAction : EventAction
+    {
+        [Serialize("", IsPropertySaveable.Yes, description: "The Identifier of the map feature to search for.")]
+        public Identifier MapFeatureIdentifier { get; set; }
+
+        [Serialize(0, IsPropertySaveable.Yes, description: "The max distance this map feature can be away from the current location (1 = one path between locations).")]
+        public int MaxDistance { get; set; }
+
+        [Serialize(false, IsPropertySaveable.Yes, description: "If true, a suitable location is forced on the map if one isn't found.")]
+        public bool CreateFeatureIfNotFound { get; set; }
+
+
+
+        public RevealMapFeatureAction(ScriptedEvent parentEvent, ContentXElement element) : base(parentEvent, element)
+        {
+        }
+
+        public override bool IsFinished(ref string goToLabel) => throw new System.NotImplementedException();
+        public override void Reset() => throw new System.NotImplementedException();
+    }
+}

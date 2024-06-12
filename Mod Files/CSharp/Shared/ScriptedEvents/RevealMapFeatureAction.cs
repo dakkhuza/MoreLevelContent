ff6fe65fb2a4a9110dfdcde8c2849fc09ca7b855
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-06-12 01:45:55 -0700

    Code for revealing map features

--- Mod Files/CSharp/Shared/ScriptedEvents/RevealMapFeatureAction.cs
+++ Mod Files/CSharp/Shared/ScriptedEvents/RevealMapFeatureAction.cs
@@ -2,6 +2,8 @@
 using Barotrauma;
 using MoreLevelContent.Shared.Data;
 using MoreLevelContent.Shared.Generation;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MoreLevelContent
 {
@@ -14,18 +16,96 @@ namespace MoreLevelContent
         public Identifier MapFeatureIdentifier { get; set; }
 
         [Serialize(0, IsPropertySaveable.Yes, description: "The max distance this map feature can be away from the current location (1 = one path between locations).")]
-        public int MaxDistance { get; set; }
+        public int MinDistance { get; set; }
 
-        [Serialize(false, IsPropertySaveable.Yes, description: "If true, a suitable location is forced on the map if one isn't found.")]
-        public bool CreateFeatureIfNotFound { get; set; }
-
+        private bool isFinished = false;
 
 
         public RevealMapFeatureAction(ScriptedEvent parentEvent, ContentXElement element) : base(parentEvent, element)
         {
+            if (MapFeatureIdentifier.IsEmpty)
+            {
+                DebugConsole.ThrowError($"Error in event \"{parentEvent.Prefab.Identifier}\": MapFeatureIdentifier has not been configured.",
+                    contentPackage: element.ContentPackage);
+            }
         }
 
-        public override bool IsFinished(ref string goToLabel) => throw new System.NotImplementedException();
-        public override void Reset() => throw new System.NotImplementedException();
+
+
+        public override void Update(float deltaTime)
+        {
+            if (GameMain.GameSession.GameMode is CampaignMode campaign)
+            {
+                var featureLocation = FindConnectionWithMapFeature(MapFeatureIdentifier, MinDistance);
+
+                if (featureLocation != null)
+                {
+                    featureLocation.LevelData.MLC().MapFeatureData.Revealed = true;
+
+                    // Probably have to do some syncing here, maybe not
+                    if (campaign is MultiPlayerCampaign mpCampaign)
+                    {
+                        mpCampaign.IncrementLastUpdateIdForFlag(MultiPlayerCampaign.NetFlags.MapAndMissions);
+                    }
+                    foreach (var location in featureLocation.Locations)
+                    {
+                        campaign.Map.Discover(location, checkTalents: false);
+                    }
+                }
+            }
+
+            isFinished = true;
+        }
+
+        private LocationConnection FindConnectionWithMapFeature(Identifier name, int minDistance)
+        {
+            var campaign = GameMain.GameSession.GameMode as CampaignMode;
+
+            var currentConnection = campaign.Map.SelectedConnection;
+            int distance = 0;
+            HashSet<LocationConnection> checkedConnections = new HashSet<LocationConnection>();
+            HashSet<LocationConnection> pendingConnections = new HashSet<LocationConnection>() { currentConnection };
+
+
+            do
+            {
+                List<LocationConnection> currentConnections = pendingConnections.ToList();
+                pendingConnections.Clear();
+
+                foreach (var connection in currentConnections)
+                {
+                    checkedConnections.Add(connection);
+                    var feature = connection.LevelData.MLC().MapFeatureData;
+                    if (feature.Name == name && !feature.Revealed)
+                    {
+                        return connection;
+                    }
+                    else
+                    {
+                        foreach (Location location in connection.Locations)
+                        {
+                            foreach (var item in location.Connections)
+                            {
+                                if (checkedConnections.Contains(item)) { continue;  }
+                                pendingConnections.Add(item);
+                            }
+                        }
+                    }
+                }
+
+
+            } while (pendingConnections.Any());
+
+
+            return null;
+        }
+
+        public override bool IsFinished(ref string goToLabel) => isFinished;
+        public override void Reset() => isFinished = false;
+        public override string ToDebugString()
+        {
+            return $"{ToolBox.GetDebugSymbol(isFinished)} {nameof(RevealMapFeatureAction)} -> ({(MapFeatureIdentifier)})";
+        }
+
     }
 }

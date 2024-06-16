24dddbf624c62512d4a1eedf39ea1f2f2fcb73dd
Dak <48874322+dakkhuza@users.noreply.github.com>
2024-06-15 23:59:53 -0700

    Update PatchNotes.cs

--- Mod Files/CSharp/Client/UI/PatchNotes.cs
+++ Mod Files/CSharp/Client/UI/PatchNotes.cs
@@ -18,7 +18,7 @@ namespace Barotrauma.MoreLevelContent.Client.UI
             backgroundBlocker = new GUIFrame(new RectTransform(Vector2.One, Screen.Selected.Frame.RectTransform, Anchor.Center), style: null);
             _ = new GUIFrame(new RectTransform(GUI.Canvas.RelativeSize, backgroundBlocker.RectTransform, Anchor.Center), style: "GUIBackgroundBlocker");
 
-            var mainParent = new GUIFrame(new RectTransform(new Vector2(0.5f, 0.4f), backgroundBlocker.RectTransform, Anchor.Center, scaleBasis: ScaleBasis.Smallest) { MinSize = new Point(640, 480) }).RectTransform;
+            var mainParent = new GUIFrame(new RectTransform(new Vector2(0.5f, 0.55f), backgroundBlocker.RectTransform, Anchor.Center, scaleBasis: ScaleBasis.Smallest) { MinSize = new Point(640, 480) }).RectTransform;
 
             mainFrame = new GUIFrame(new RectTransform(Vector2.One, mainParent));
             var mainLayout = new GUILayoutGroup(new RectTransform(Vector2.One * 0.95f, mainFrame.RectTransform, Anchor.Center, Pivot.Center),
